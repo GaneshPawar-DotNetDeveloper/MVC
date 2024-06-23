25d5aebc69c12897fdccee32e28df3a7a8801441
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-06-23 21:21:28 +0530

    fds

---
+++ HelperClass/Common/CustomHTMLHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperClass.Common
+{
+    public static class CustomHTMLHelper
+    {
+        // here we can write a extension method
+        // in that method we can write a logic
+        public static MvcHtmlString Image(this HtmlHelper html,string src)
+        {
+            TagBuilder image=new TagBuilder("img");
+            image.Attributes.Add("src", src);
+            return new MvcHtmlString(image.ToString());
+        }
+
+    }
+}
