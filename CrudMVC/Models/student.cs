1f889aee0bbf41db73594c7d4307f1fafa56d24b
Ganesh Pawar <165685426+GaneshPawar-DotNetDeveloper@users.noreply.github.com>
2024-07-01 23:59:11 +0530

    Add files via upload

---
+++ CrudMVC/Models/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudMVC.Models
+{
+    public class student
+    {
+        public int RollNumber { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
